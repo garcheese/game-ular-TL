6ae32683689df210ab59b4aee0f709b941f20b92
garcheese <gartegarte03@gmail.com>
2025-05-22 12:05:52 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,24 @@
-const CACHE_NAME = "snake-cache-v1";
+const CACHE_NAME = "game-ular-tl-v1";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/manifest.json",
-  "/icon-192.png",
-  "/icon-512.png",
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
+// Install service worker dan cache file
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
+// Ambil dari cache saat offline
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
